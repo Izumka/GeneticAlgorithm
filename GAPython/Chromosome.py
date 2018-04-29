b64a5f3f95916ffc1c187ab9dff33e927df3caec
mhirna <m.hirna@ucu.edu.ua>
2018-04-29 23:10:44 +0300

    add crossover & mutation

--- GAPython/Chromosome.py
+++ GAPython/Chromosome.py
@@ -3,45 +3,60 @@ import random
 class Chromosome:
     def __init__(self, **kwargs):
         self.length = kwargs["length"]
-#        self.genes = kwargs["genes"]
-        self.fitness = 0
         self.fitness_fun = kwargs["fitness_fun"]
-        self.num_type = kwargs["num_type"]
-        print(self.num_type)
-
-    def get_fitness(self):
-        return self.fitness_fun(self.genes)
-
-    def get_genes(self):
-        if self.num_type == "bin":
-            self.genes = [random.randint(0, 1) for i in range(self.length)]
-        elif self.num_type == "dec":
-            self.genes = [random.randint(0, 9) for i in range(self.length)]
-        elif self.num_type == "int":
-            tmp_genes = [i for i in range(self.length)]
-            self.genes = random.shuffle(tmp_genes)
-            print(self.genes)
-        #return self.genes
+        if "genes" in kwargs:
+            self.genes = kwargs["genes"]
+        else:
+            self.genes = random.sample([i for i in range(0, self.length)], self.length)
+        self.fitness = self.fitness_fun(self.genes)
 
     def __str__(self):
         return str(self.genes)
 
     def crossover(self, other):
         parents = {"parent_1": self, "parent_2": other}
+        new_genes_1 = [-1] * self.length
+        new_genes_2 = [-1] * self.length
 
         # Define who has better fitness & assign better fitness to parent 1
         if self.fitness < other.fitness:
             parents["parent_1"], parents["parent_2"] = parents["parent_2"], parents["parent_1"]
 
         # Generate random start and random length of gene copied
-        mut_length = random.randint(1, 8)
+        mut_length = random.randint(1, self.length - 1)
         mut_start = random.randint(0, self.length - mut_length - 1)
-        new_genes = [0] * self.length
 
         # Put part of parent one gene into new gene
-        new_genes = new_genes[:mut_start] + parents["parent_1"].genes[mut_start: mut_start + mut_length + 1] + new_genes[mut_start + mut_length:]
+        new_genes_1 = new_genes_1[:mut_start] + parents["parent_1"].genes[mut_start: mut_start + mut_length] + new_genes_1[mut_start + mut_length:]
+        new_genes_2 = new_genes_2[:mut_start] + parents["parent_2"].genes[mut_start: mut_start + mut_length] + new_genes_2[mut_start + mut_length:]
 
-        return
+        for i in parents["parent_2"].genes:
+            if i not in new_genes_1:
+                new_genes_1[new_genes_1.index(-1)] = i
+
+        for j in parents["parent_1"].genes:
+            if j not in new_genes_2:
+                new_genes_2[new_genes_2.index(-1)] = j
+
+        length = self.length
+        fitness_fun = self.fitness_fun
+
+        child_1 = Chromosome(length=length, genes=new_genes_1, fitness_fun=fitness_fun)
+        child_2 = Chromosome(length=length, genes=new_genes_2, fitness_fun=fitness_fun)
+        return (child_1, child_2)
 
     def mutation(self):
-        return
+        swap_1 = random.randint(0, self.length - 1)
+        swap_2 = random.randint(0, self.length - 1)
+        self.genes[swap_1], self.genes[swap_2] = self.genes[swap_2], self.genes[swap_1]
+
+def fit(lst):
+    n = len(lst)
+    return sum([((i + 1) / n) * lst[i] for i in range(n)])
+
+ch = Chromosome(length=8, fitness_fun=fit)
+ch2 = Chromosome(length=8, fitness_fun=fit)
+children = ch.crossover(ch2)
+print(children[0].genes)
+print(children[1].genes)
+
