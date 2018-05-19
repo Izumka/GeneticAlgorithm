4d5b863a4d80fcd399a555c653fa4c6e18867afd
famdrum <ko.oles999@gmail.com>
2018-05-19 13:49:25 +0300

    Chromosome class

---
+++ cmake-build-debug/CMakeFiles/chromosome.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chromosome.dir/main.cpp.obj"
+  "chromosome.pdb"
+  "chromosome.exe"
+  "chromosome.exe.manifest"
+  "libchromosome.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chromosome.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
