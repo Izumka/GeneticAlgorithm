0dfe17dc881ae4033c5b054690d4f17841d7a208
Izumka <fy0ysvec>
2018-05-23 21:01:55 +0300

    add .gitignore

--- main.cpp
+++ main.cpp
@@ -7,32 +7,12 @@
 #include <thread>
 //#include "chromosome.h"
 //#include "population.h"
+
 using std::vector;
 using namespace std;
 
-void func(int i, int& res){
-//    for (int j = 0; j < 100; j++) {
-//        std::cout << "index: " << i << std::endl;
-//    }
-
-    res = i+2;
-}
 
 int main() {
-    vector<std::thread> threads;
-    vector<int> results{2};
-
-    for (int i = 0; i < 2; i++) {
-        threads.push_back(std::thread(func, i, results[i]));
-    }
-
-    for (int i = 0; i < 2; i++) {
-        threads[i].join();
-    }
-
-    for (int i = 0; i < 2; i++) {
-        cout << results[i] << endl;
-    }
 
     return 0;
 }
